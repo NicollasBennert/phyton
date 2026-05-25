a6e5fd512c551fdc497ab44738e9f762bd7f6545
NicollasBennert <nicollasbennert1@gmail.com>
2026-05-25 15:15:27 -0300

    Calculadora criada e instanciada

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+def soma(a,b):
+    return print(a + b )
+
+def subtraçao(a,b):
+    return print(a-b)
+
+
+def multiplicaçao(a,b):
+    return print(a*b)
+
+def divisao(a,b):
+    return print(a/b)
